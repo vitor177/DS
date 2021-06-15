f207186168fcd202b38ac21b69a9d3ef8a34d22c
João Vítor <joovitor177@gmail.com>
2021-06-15 17:03:40 -0300

    Pipeline

--- src/treino_celulas_pipeline.py
+++ src/treino_celulas_pipeline.py
@@ -7,3 +7,24 @@ from feature_engine.encoding import OneHotEncoder
 # %%
 df_celula = pd.read_csv("../data/celulas.csv", sep=";")
 df_celula
+# %%
+features = ["nucleos", "caudas", "cor", "membrana"]
+target = "classe"
+
+onehot = OneHotEncoder(variables=["cor", "membrana"])
+
+clf_tree = tree.DecisionTreeClassifier()
+
+model_pipeline = pipeline.Pipeline(steps= [("Onehot", onehot),
+                                    ("Tree", clf_tree)])
+
+model_pipeline.fit(df_celula[features], df_celula[target])
+# %%
+model = pd.Series( {
+    "model": model_pipeline,
+    "features": features,
+    "target":target
+})
+
+model.to_pickle("../models/celulas_tree_pipeline.pkl")
+# %%
